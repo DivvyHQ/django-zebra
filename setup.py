fef55c2ab3b43192bcef55eb61dc41b1b33ddff4
Collin <collin@divvyhq.com>
2020-01-24 15:57:44 -0600

    Change version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author = "DivvyHQ",
     author_email = "contact@divvyhq.com",
     url = "https://github.com/DivvyHQ/django-zebra",
-    version = "2.2",
+    version = "2.3",
     packages = find_packages(),
     zip_safe = False,
     include_package_data=True,
